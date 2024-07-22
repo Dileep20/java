b72087dc4bedf728a69bac306b46849c880eb4b6
dileep20 <dileep.xworkz@gmail.com>
2024-07-22 22:48:44 +0530

    create and read code

--- ZomatoRunner.java
+++
@@ -1,7 +0,0 @@
-class ZomatoRunner{
-	public static void main(String[] item){
-		double price = Swiggy.search("Chicken Lollipop");
-		System.out.println("Chicken Lollipop price is "+price);
-		
-	}
-}
