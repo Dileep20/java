b9473faea8d08c0ceca8f5427ef5c45a2b864481
dileep20 <dileep.xworkz@gmail.com>
2024-07-30 22:02:00 +0530

    object creation

---
+++ Bill.java
@@ -0,0 +1,11 @@
+class Bill{
+	int billReferenceId; 
+	int billSequenceNo; 
+	boolean isLatestBill; 
+	boolean isFinalBill; 
+	double billAmount; 
+	boolean taxInclusiveBill;
+	String billIssueDate; 
+	String billDueDate; 
+	boolean isBillPaid;
+}
